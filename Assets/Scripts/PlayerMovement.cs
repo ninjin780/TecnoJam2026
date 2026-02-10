60514ba0cbe4751328e34939a2c86031deb11755
ninjin780 <ninjin780@gmail.com>
2026-02-10 17:14:17 +0100

    Organizacion

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 5.0f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate() // Mejor usar FixedUpdate para físicas
+    {
+        rb.linearVelocity = moveInput * Speed;
+
+        if (moveInput != Vector2.zero)
+        {
+           
+            if (moveInput.x != 0)
+            {
+                transform.localScale = new Vector3(-Mathf.Sign(moveInput.x), 1, 1);
+            }
+            animator.SetFloat("horizontal", moveInput.x);
+            animator.SetFloat("vertical", moveInput.y);
+            animator.SetBool("isMoving", true);
+
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
