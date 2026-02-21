325614f3ff2c013dd09bcc997f490fe8ea4d42c8
Arnau Badenas <4bvives@gmail.com>
2026-02-21 14:44:01 +0100

    Make objects interactable
    object has interactable script
    player has interactor script

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Interactable : MonoBehaviour, IInteractable
+    {
+
+        public void Interact()
+        {
+            Debug.Log("Interacted with " + gameObject);
+            //DialogueManager.StartDialogue();
+        }
+    }
+}
