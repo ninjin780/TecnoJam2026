77021b006a8d8376933c9fbaa0bb4db817785dac
Carla Pallés Ros <carlaa.pr15@gmail.com>
2026-02-28 22:37:19 +0100

    Cámara sigue al jugador con bounds

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,26 +4,37 @@ using UnityEngine.InputSystem;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset = new(0, 0, -10);
+    public BoxCollider2D mapBounds;
 
-    Vector2 pos;
-    Vector2 targetPos;
-
-    InputAction moveAction;
+    private float minX, minY, maxX, maxY;
+    private float camX, camY;
+    private float camSize;
+    private float camRatio;
+    private Camera cam;
 
     private void Start()
     {
-        moveAction = InputSystem.actions.FindAction("Car/Move");
+        minX = mapBounds.bounds.min.x;
+        minY = mapBounds.bounds.min.y;
+        maxX = mapBounds.bounds.max.x;
+        maxY = mapBounds.bounds.max.y;
+
+        cam = GetComponent<Camera>();
+        camSize = cam.orthographicSize;
+        camRatio = (maxX + camSize) / 2.0f;
     }
 
     void LateUpdate()
     {
+        camY = Mathf.Clamp(target.position.y, minY + camSize, maxY - camSize);
+        camX = Mathf.Clamp(target.position.x, minX + camSize, maxX - camSize);
+
         if (target != null)
         {
             transform.position = new Vector3(
-                transform.position.x,
-                target.position.y + offset.y,
-                offset.z
+                camX,
+                camY,
+                transform.position.z
                 );
         }
     }
