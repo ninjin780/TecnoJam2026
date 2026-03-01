186eb0c51cd38277b599f32510be4558ae3c9606
Carla Pallés Ros <carlaa.pr15@gmail.com>
2026-03-01 23:03:23 +0100

    Mapa exterior + no se mueve al chocarse

--- Assets/Scripts/Dialogue/DialogeOutOfBounds.cs
+++ Assets/Scripts/Dialogue/DialogeOutOfBounds.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class DialogeOutOfBounds : MonoBehaviour
 {
     [SerializeField] private DialogueRoundSO dialogue;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void OnTriggerExit2D(Collider2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         DialogueManager.Instance.StartDialogue(dialogue);
     }
