db4a970e60de2fe787e29875444534ed74a65f27
Carla Pallés Ros <carlaa.pr15@gmail.com>
2026-03-01 18:27:29 +0100

    Cambios al canvas

--- Assets/Scripts/Notification/NotificationSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewNotification", menuName = "Notifications")]
-public class NotificationSO : ScriptableObject
-{
-    [TextArea] public string Notification;
-    public float TextDuration = 2.0f;
-    public float FadeDuration = 1.0f;
-}
